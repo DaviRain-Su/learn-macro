cf023a393782ec7d2b0867d788024d9b81b893a5
Davirain <davirain.yin@gmail.com>
2022-08-24 13:32:19 +0800

    add attribute

--- builder/examples/command.rs
+++ builder/examples/command.rs
@@ -4,16 +4,19 @@ use builder::Builder;
 #[derive(Debug, Builder)]
 pub struct Command {
     executable: String,
+    #[builder(each = "arg", default = "Default::default()")]
     args: Vec<String>,
+    #[builder(each = "env", default = "vec![\"RUST_LOG=info\".into()]")]
     env: Vec<String>,
-    current_dir: String,
+    current_dir: Option<String>,
 }
 
 fn main() {
     let command = Command::builder()
         .executable("find")
-        .args(vec!["-c".into(), "-vv".into()])
-        .env(vec![])
+        // .arg("-c")
+        // .arg("-vv")
+        // .env("RUST_LOG=info")
         .current_dir("/user/davirain")
         .finish();
 
--- builder/src/builder.rs
+++ builder/src/builder.rs
@@ -1,20 +1,40 @@
 use std::iter::Map;
+use std::slice::Iter;
 
 use proc_macro2::Ident;
 use proc_macro2::TokenStream;
 use quote::quote;
+use syn::GenericArgument;
+use syn::Path;
+use syn::PathArguments;
+use syn::Type;
+use syn::TypePath;
+use darling::FromField;
 use syn::{
-    punctuated::{Iter, Punctuated},
-    token::Comma,
-    Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed,
+    Data, DataStruct, DeriveInput, Fields, FieldsNamed,
 };
 
-type TokenStreamIter<'a> = Map<Iter<'a, Field>, fn(&'a Field) -> TokenStream>;
+type TokenStreamIter<'a> = Map<Iter<'a, Fd>, fn(&'a Fd) -> TokenStream>;
+
+#[derive(Debug, Default, FromField)]
+#[darling(default, attributes(builder))]
+struct Opts {
+    each: Option<String>,
+    default: Option<String>,
+}
+
+#[derive(Debug)]
+struct Fd {
+    name: Ident,
+    ty: Type,
+    opts: Opts,
+}
+
 
 #[derive(Debug)]
 pub struct BuilderContext {
     name: Ident,
-    fields: Punctuated<Field, Comma>,
+    fields: Vec<Fd>,
 }
 
 impl BuilderContext {
@@ -30,7 +50,15 @@ impl BuilderContext {
             panic!("Unsupported data type");
         };
 
-        Self { name, fields }
+        let fds = fields.into_iter().map(|f| {
+            Fd {
+                opts: Opts::from_field(&f).unwrap_or_default(),
+                name: f.ident.unwrap(),
+                ty: f.ty,
+            }
+        }).collect();
+   
+        Self { name, fields: fds }
     }
 
     pub fn generate(&self) -> TokenStream {
@@ -74,16 +102,32 @@ impl BuilderContext {
 
     fn gen_optionized_fields(&self) -> TokenStreamIter {
         self.fields.iter().map(|f| {
-            let ty = &f.ty;
-            let name = &f.ident;
+            
+            let (_, ty) = get_option_inner(&f.ty);
+            let name = &f.name;
             quote! { #name: std::option::Option<#ty> }
         })
     }
 
     fn gen_methods(&self) -> TokenStreamIter {
         self.fields.iter().map(|f| {
-            let ty = &f.ty;
-            let name = &f.ident;
+            let (_, ty) = get_option_inner(&f.ty);
+            let (is_vec, vec_inner_type) = get_vec_inner(&f.ty);
+            let name = &f.name;
+            if is_vec {
+                if let Some(each_name) = f.opts.each.as_deref() {
+                    let each_name = Ident::new(each_name, f.name.span());
+                    return   quote! {
+                        pub fn #each_name(mut self, v: impl Into<#vec_inner_type>) -> Self { 
+                            let mut data = self.#name.take().unwrap_or_default();
+                            data.push(v.into());
+                            self.#name = Some(data);
+                            self
+                        }
+                    };
+                }
+            }
+
             // option fields. e.g. executable: String -> executable: Option<String>
             quote! {
                 pub fn #name(mut self, v: impl Into<#ty>) -> Self {
@@ -96,11 +140,52 @@ impl BuilderContext {
 
     fn gen_assigns(&self) -> TokenStreamIter {
         self.fields.iter().map(|f| {
-            let name = &f.ident;
+            let name = &f.name;
+            let (optional, _) = get_option_inner(&f.ty);
+            if optional {
+                return quote! {
+                    #name: self.#name.take()
+                };
+            }
+
+            if let Some(default) = f.opts.default.as_deref() {
+                let ast : TokenStream = default.parse().unwrap();
+                return quote! { #name: self.#name.take().unwrap_or_else(|| #ast)}
+            }
+
             // field_name: self.#field_name.take().ok_or(" xx need to be set!")
             quote! {
                 #name: self.#name.take().ok_or(concat!(stringify!(#name), " needs to be set!"))?
             }
         })
     }
+}
+
+fn get_option_inner(ty: &Type) -> (bool, &Type) {
+    get_type_inner(ty, "Option")
+}
+
+
+fn get_vec_inner(ty: &Type) -> (bool, &Type) {
+    get_type_inner(ty, "Vec")
+}
+
+
+fn get_type_inner<'a>(ty: &'a Type, name: &str) -> (bool, &'a Type) {
+    if let Type::Path(TypePath { path: Path {segments, ..}, ..}) = ty {
+        if let Some(v) = segments.first() {
+            if v.ident == name {
+                let t = match &v.arguments {
+                    PathArguments::AngleBracketed(a) => match a.args.iter().next() {
+                        Some(GenericArgument::Type(t)) => t,
+                        _ => panic!("Not sure what to do with other GenericArgument"),
+                    },
+                    _ => panic!("Not sure what to do with other PathArguments"),
+                };
+                return (true, t);   
+            }
+        }
+    }
+    
+    return (false, ty);
 }
